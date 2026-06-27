038695ffaaa8ec27bb67dc77ae987b377eff812c
Madabila <votersueu3@gmail.com>
2026-06-27 20:34:23 +0700

    Fix router for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Vendors from './pages/Vendors';
